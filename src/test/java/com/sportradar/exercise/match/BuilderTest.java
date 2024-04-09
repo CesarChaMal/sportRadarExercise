ab950002637f4255a6c69135e814c887d75edcfb
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2024-04-09 11:56:40 +0200

    Adding more test for the builder test.

--- src/test/java/com/sportradar/exercise/match/BuilderTest.java
+++ src/test/java/com/sportradar/exercise/match/BuilderTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BuilderTest {
 
@@ -19,5 +20,19 @@ public class BuilderTest {
         Match match = builder.build();
         assertEquals("Team A should be set as home team", "Team A", match.getHomeTeam());
         assertEquals("Team B should be set as away team", "Team B", match.getAwayTeam());
+    }
+
+    @Test
+    public void testBuilderInitializesScoresToZero() {
+        Match match = builder.build();
+        assertEquals("Initial home score should be 0", 0, match.getHomeScore());
+        assertEquals("Initial away score should be 0", 0, match.getAwayScore());
+    }
+
+    @Test
+    public void testBuilderAppliesCustomScore() {
+        Match match = builder.homeScore(1).awayScore(2).build();
+        assertEquals("Home score should be set to 1", 1, match.getHomeScore());
+        assertEquals("Away score should be set to 2", 2, match.getAwayScore());
     }
 }
